418bfb71ae21044f4ee7e9b6674b5f51ffe2c74a
Lois Kilberg <46911988+loiskilberg@users.noreply.github.com>
2019-02-23 16:50:10 -0500

    add built-in themes to choose from, try agogo

--- source/conf.py
+++ source/conf.py
@@ -21,10 +21,18 @@ language = None
 
 pygments_style = 'sphinx'
 
-
+#Here's where to find some themes: https://www.sphinx-doc.org/en/master/usage/theming.html?highlight=theme
 #html_theme = 'default'
+html_theme = 'agogo'
 #html_theme = 'alabaster'
-html_theme = 'haiku'
+#html_theme = 'bizstyle'
+#html_theme = 'classic'
+#html_theme = 'haiku'
+#html_theme = 'nature'
+#html_theme = 'pyramid'
+#html_theme = 'scrolls'
+#html_theme = 'sphinxdoc'
+#html_theme = 'traditional'
 
 rst_epilog = """
 .. include:: substitutions.txt
